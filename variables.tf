fb64fb1a4f2861043ec88284ebd6aa8ae12f471e
Rahul Chordiya <rahulchordiya@gmail.com>
2023-06-26 23:11:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,3 +16,13 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+
+variable "subnet_id_1" {
+  type = string
+  default = "subnet-your_first_subnet_id"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-your_second_subnet_id"
+ }
